df87c1ec25234e68bdebfd10e305d4524d6e72fc
Shreyansh Agarwal <shreyanshag@gmail.com>
2017-12-23 23:50:16 +0530

    Add room activities and required files

--- app/src/main/java/com/champ/sa/smarthome/MainActivity.java
+++ app/src/main/java/com/champ/sa/smarthome/MainActivity.java
@@ -1,37 +1,106 @@
 package com.champ.sa.smarthome;
 
-        import android.app.Activity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Toast;
-        import android.widget.ToggleButton;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.content.IntentFilter;
 
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    ImageButton room1;
+    ImageButton room2;
+    ImageButton room3;
+    ImageButton room4;
+    ImageButton room5;
 
-    ToggleButton tbutton;
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Really Exit?")
+                .setMessage("Are you sure you want to exit?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        MainActivity.super.onBackPressed();
+                    }
+                }).create().show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        room1 = (ImageButton) findViewById(R.id.room1);
+        room2 = (ImageButton) findViewById(R.id.room2);
+        room3 = (ImageButton) findViewById(R.id.room3);
+        room4 = (ImageButton) findViewById(R.id.room4);
+        room5 = (ImageButton) findViewById(R.id.room5);
 
-        tbutton = (ToggleButton)findViewById(R.id.toggleButton1);
 
-        tbutton.setOnClickListener(new View.OnClickListener() {
+        room1.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Drawing Room", Toast.LENGTH_LONG).show();
+                Intent o = new Intent(MainActivity.this, Room1.class);
+                startActivity(o);
 
-                if(tbutton.isChecked())
-                {
-                    Toast.makeText(MainActivity.this, "Toggle button is on", Toast.LENGTH_LONG).show();
-
-                }
-                else {
-                    Toast.makeText(MainActivity.this, "Toggle button is Off", Toast.LENGTH_LONG).show();
-                }
             }
         });
+
+        room2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Master Bed Room", Toast.LENGTH_LONG).show();
+                Intent o = new Intent(MainActivity.this, Room2.class);
+                startActivity(o);
+
+            }
+        });
+
+        room3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Kitchen", Toast.LENGTH_LONG).show();
+                Intent o = new Intent(MainActivity.this, Room3.class);
+                startActivity(o);
+
+            }
+        });
+
+        room4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Children Bed Room", Toast.LENGTH_LONG).show();
+                Intent o = new Intent(MainActivity.this, Room4.class);
+                startActivity(o);
+
+            }
+        });
+
+
+        room5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Balcony and Lobby", Toast.LENGTH_LONG).show();
+                Intent o = new Intent(MainActivity.this, Room5.class);
+                startActivity(o);
+
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/champ/sa/smarthome/Room2.java
@@ -0,0 +1,100 @@
+package com.champ.sa.smarthome;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+
+public class Room2 extends AppCompatActivity {
+    ToggleButton button1;
+    ToggleButton button2;
+    ToggleButton button3;
+    ToggleButton button4;
+    ToggleButton button5;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room2);
+
+        button1=(ToggleButton)findViewById(R.id.lightToggle);
+        button2=(ToggleButton)findViewById(R.id.fanToggle);
+        button3=(ToggleButton)findViewById(R.id.acToggle);
+        button4=(ToggleButton)findViewById(R.id.sensingToggle);
+        button5=(ToggleButton)findViewById(R.id.nightLampToggle);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (button1.isChecked()) {
+                    Toast.makeText(Room2.this, "Light's ON!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Room2.this, "Light's OFF!", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (button2.isChecked()) {
+                    Toast.makeText(Room2.this, "Fan ON!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Room2.this, "Fan OFF!", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (button3.isChecked()) {
+                    Toast.makeText(Room2.this, "Air conditioning ON!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Room2.this, "Air conditioning OFF!", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (button4.isChecked()) {
+                    Toast.makeText(Room2.this, "Automatic sensing ON!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Room2.this, "Automatic sensing OFF!", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (button5.isChecked()) {
+                    Toast.makeText(Room2.this, "Night Lamp ON!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Room2.this, "Night Lamp OFF!", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+    }
+}
